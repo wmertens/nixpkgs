f2bf1c94880a363d46636fbb04ca942b5f0aa258
Daniel Peebles <pumpkin@me.com>
2014-12-18 22:44:42 -0500

    Fix libusb

--- pkgs/development/libraries/libusb/default.nix
+++ pkgs/development/libraries/libusb/default.nix
@@ -6,8 +6,6 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ pkgconfig ];
   propagatedBuildInputs = [ libusb1 ];
 
-  buildInputs = stdenv.lib.optional stdenv.isDarwin iokit;
-
   src = fetchurl {
     url = mirror://sourceforge/libusb/libusb-compat-0.1.5.tar.bz2;
     sha256 = "0nn5icrfm9lkhzw1xjvaks9bq3w6mjg86ggv3fn7kgi4nfvg8kj0";
