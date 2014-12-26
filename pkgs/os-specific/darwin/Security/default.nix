389d720fa314d606f2d7a6dec3c99bbc42e3de0e
Daniel Peebles <pumpkin@me.com>
2014-12-26 14:18:51 -0500

    Impure Security.framework + pure headers

--- pkgs/os-specific/darwin/Security/default.nix
+++ pkgs/os-specific/darwin/Security/default.nix
@@ -1,19 +1,32 @@
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  version = "57031.1.35";
+  version = "55471.14.18";
   name    = "Security-${version}";
 
   src = fetchurl {
     url    = "http://www.opensource.apple.com/tarballs/Security/${name}.tar.gz";
-    sha256 = "0aamxscaggyymw97134rl4s0qjj3jfr69g88r4cjs9nvgwqdgan3";
+    sha256 = "1nv0dczf67dhk17hscx52izgdcyacgyy12ag0jh6nl5hmfzsn8yy";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
 
   installPhase = ''
-    mkdir -p $out/System/Library/Frameworks/Security.framework/Headers
-    cp Security/sec/Security/*.h $out/System/Library/Frameworks/Security.framework/Headers/
-    cp Security/libsecurity_cssm/lib/*.h $out/System/Library/Frameworks/Security.framework/Headers/
+    ###### IMPURITIES
+    mkdir -p $out/Library/Frameworks/Security.framework
+    pushd $out/Library/Frameworks/Security.framework
+    ln -s /System/Library/Frameworks/Security.framework/Security
+    ln -s /System/Library/Frameworks/Security.framework/Resources
+    ln -s /System/Library/Frameworks/Security.framework/PlugIns
+    ln -s /System/Library/Frameworks/Security.framework/XPCServices
+    popd
+
+    ###### HEADERS
+
+    export dest=$out/Library/Frameworks/Security.framework/Headers
+    mkdir -p $dest
+    cp sec/Security/*.h                  $dest
+    cp libsecurity_authorization/lib/*.h $dest
+    cp libsecurity_cssm/lib/*.h          $dest
   '';
 }
