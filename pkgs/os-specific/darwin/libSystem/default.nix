0023e484d0ea49a13dee61fc429de4e4f6d2cb87
Daniel Peebles <pumpkin@me.com>
2015-01-15 03:08:28 -0500

    Add some more TargetConditionals

--- pkgs/os-specific/darwin/libSystem/default.nix
+++ pkgs/os-specific/darwin/libSystem/default.nix
@@ -74,6 +74,21 @@ stdenv.mkDerivation rec {
     #define TARGET_OS_IPHONE        0
     #define TARGET_IPHONE_SIMULATOR 0
     #define TARGET_OS_LINUX         0
+
+    #define TARGET_CPU_PPC          0
+    #define TARGET_CPU_PPC64        0
+    #define TARGET_CPU_68K          0
+    #define TARGET_CPU_X86          0
+    #define TARGET_CPU_X86_64       1
+    #define TARGET_CPU_ARM          0
+    #define TARGET_CPU_MIPS         0
+    #define TARGET_CPU_SPARC        0
+    #define TARGET_CPU_ALPHA        0
+    #define TARGET_RT_MAC_CFM       0
+    #define TARGET_RT_MAC_MACHO     1
+    #define TARGET_RT_LITTLE_ENDIAN 1
+    #define TARGET_RT_BIG_ENDIAN    0
+    #define TARGET_RT_64_BIT        1
     #endif  /* __TARGETCONDITIONALS__ */
     EOF
 
